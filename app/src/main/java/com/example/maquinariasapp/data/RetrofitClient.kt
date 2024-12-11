3ae73dcb26451f9e1bf0fef20a68e29847bd0013
amoroccoire <amoroccoire@unsa.edu.pe>
2024-12-10 22:18:47 -0500

    retrofit client

---
+++ app/src/main/java/com/example/maquinariasapp/data/RetrofitClient.kt
@@ -0,0 +1,15 @@
+package com.example.maquinariasapp.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://negocios-electronicos-laboratorio8.onrender.com/api/"
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
