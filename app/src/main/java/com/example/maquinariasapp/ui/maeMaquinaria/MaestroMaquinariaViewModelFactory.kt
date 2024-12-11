d93df1f13ac31ce1a2101cf57947a36e6b707027
amoroccoire <amoroccoire@unsa.edu.pe>
2024-12-10 22:24:39 -0500

    clases con potencial uso

---
+++ app/src/main/java/com/example/maquinariasapp/ui/maeMaquinaria/MaestroMaquinariaViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.example.maquinariasapp.ui.maeMaquinaria
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+/*class MaestroMaquinariaViewModelFactory(private val context: Context) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(MaestroMaquinariaViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return MaestroMaquinariaViewModel(context) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}*/
+class MaestroMaquinariaViewModelFactory(){
+}
